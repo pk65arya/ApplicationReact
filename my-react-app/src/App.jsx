2fa8c0ad99f1d5c4bd06531d57a43128d4dac3d1
pk65arya <pk65arya@gmail.com>
2024-06-12 13:55:57 +0530

    Props in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,8 @@
 // import Footer from "./Footer.jsx";
 // import Food from "./Food.jsx";
 //import Card from "./Card.jsx";
-import Button from "./Button.jsx";
+//import Button from "./Button.jsx";
+import Student from "./Student.jsx";
 function App() {
   return(
     <>
@@ -11,7 +12,12 @@ function App() {
     <Food></Food>
     <Footer></Footer> */}
 {/* <Card></Card> */}
-    <Button />
+    {/* <Button /> */}
+<Student name="Pradeep" age={25} isStudent={true} />
+<Student name="pankaj" age={30} isStudent={false} />
+<Student name="preetesh" age={35} isStudent={false} />
+<Student name="durgesh" age={50} isStudent={true} />
+<Student name="Neha" />
     </>
   );
 }
